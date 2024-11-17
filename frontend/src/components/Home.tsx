f5dbd91f43e22c2497abf66157ebc4f80705a6e5
Jason Antwi-Appah <hey@jasonaa.me>
2024-11-17 01:21:35 -0600

    chore: remove unneeded import

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,5 +1,4 @@
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
-import { Badge } from "@/components/ui/badge"
 import { ScrollArea } from "@/components/ui/scroll-area"
 import { pinataCidToUrl, useAllImages } from '../lib/requests'
 import { Package, User, Calendar, ImageIcon, CarTaxiFront, LoaderCircle } from 'lucide-react'
