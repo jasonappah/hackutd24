d7243451abb632e236298aecbad1d6139bf0573a
Duy Pham Le <32657584+phamleduy04@users.noreply.github.com>
2024-11-17 10:45:54 -0600

    feat: dockerize backend

--- backend/src/aiPrompt/index.ts
+++ backend/src/aiPrompt/index.ts
@@ -1,16 +1,3 @@
-// export const prompt = 
-// `You are looking for items that customer might forget in a car (wallets, keyschain, keys, backpacks, jackets, drinks, foods, airpods, etc... )
-// There might not be any items left behind
-// Do not describe the environment or an object that likely is not their (couches, seats, chairs)
-// Only list the items that you see and do not list anything else
-// Describe all the items and their color that you see the customers may have left behind
-// `;
-
-// export const prompt = `You are looking for items that customers might forget in a car (wallets, keychains, keys, backpacks, jackets, drinks, foods, AirPods, etc.)
-// Describe what you see, including the colors of the items, but do not describe the environment or any objects that are unlikely to be theirs (e.g., couches, seats, chairs)
-// If no items are present or visible, respond strictly exact and only with the word "None" (without quotation marks) and nothing else
-// Your response must strictly follow these rules`;
-
 export const prompt = `
 You are tasked with identifying items that customers might forget in a car (e.g., wallets, keys, backpacks, jackets, drinks, foods, AirPods).
 
@@ -20,30 +7,3 @@ You are tasked with identifying items that customers might forget in a car (e.g.
 - Do not add any extra words or sentences.
 
 Your response should follow these rules precisely.`;
-
-/*
-Return just the newline delimiter separated list of items that you see, but if you see nothing left behind, return only "nothing".
-This is a sample response if there is a green jacket and white airpods detected:
-- Green Jackets
-- White airpods
-Sample return if there are no items detected:
-- Nothing
-*/
-
-// export const prompt = `
-// Please analyze the provided image and identify any items that a customer might forget in a car.
-// Focus on objects such as wallets, keychains, keys, backpacks, jackets, drinks, foods, AirPods, and similar items that a customer might have left behind.
-// There may be no items left behind.
-// Do not describe the environment or any objects that are human sit on such as couches, seats, or chairs.
-// Describe all the items you see along with their colors.
-// `;
-
-// export const prompt = `Your are an AI Assistant the helps you find items that a customer might forget in a car.
-// Do not describe the environment or any objects that are human sit on such as couches, seats, or chairs.
-// The attached image is very important for the task.
-// Please analyze the provided image and identify any items that a customer might forget in a car.
-// Focus on objects such as wallets, keychains, keys, backpacks, jackets, drinks, foods, AirPods, and similar items that a customer might have left behind.
-// There are very likely no items left behind.
-// If there are no items left behind, return "nothing".
-// Describe all the items you see along with their colors.
-// `;
--- backend/src/routes/api.ts
+++ backend/src/routes/api.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 import { envConfig } from '../config';
 import { prompt } from '../aiPrompt';
 import PQueue from 'p-queue';
-import { writeFileSync, readFileSync } from 'fs';
+import { readFileSync } from 'fs';
 import { pushNotifcation } from '../pushNotification';
 
 const q = new PQueue({ concurrency: 1 });
@@ -22,7 +22,7 @@ index.post('/imageb64', async (req, res) => {
 
     res.sendStatus(201);
 
-    writeFileSync('./test.jpg', Buffer.from(image, 'base64'));
+    // writeFileSync('./test.jpg', Buffer.from(image, 'base64'));
 
 
     q.add(async () => {
