1c015ed5c2ef096cfff7b00be96fbc14a8109972
Duy Pham Le <32657584+phamleduy04@users.noreply.github.com>
2024-11-25 00:01:02 -0600

    lower limit

--- backend/src/pinata/index.ts
+++ backend/src/pinata/index.ts
@@ -52,7 +52,7 @@ const createPublicGroup = async () => {
 
 const listAllGroups = async () => await pinata.groups.list();
 
-const listAllFiles = async () => await pinata.files.list().group(envConfig.PINATA_PUBLIC_GROUP_ID).order('DESC').limit(50);
+const listAllFiles = async () => await pinata.files.list().group(envConfig.PINATA_PUBLIC_GROUP_ID).order('DESC').limit(30);
 
 
 export { uploadFile, listAllFiles, createPublicGroup, listAllGroups, uploadFileBase64 };
